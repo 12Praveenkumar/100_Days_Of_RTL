372603ade8c45c04e7520dc234e48ab7f811dfbe
12Praveenkumar <124592246+12Praveenkumar@users.noreply.github.com>
2024-09-10 13:29:59 +0530

    Create mod13upcounter.v

---
+++ mod13counter/mod13upcounter.v
@@ -0,0 +1,25 @@
+module mod13upcounter (
+input clk,rst,load,
+input [3:0] din,
+output reg [3:0] cntr
+    );
+always@(posedge clk)
+begin
+if(!rst)
+begin
+cntr<=4'b0;
+end
+else if(load)
+begin
+cntr<=din;
+end
+else if(cntr>=12)
+begin
+cntr=1'b0;
+end
+else
+begin
+cntr<=cntr+1'b1;
+end
+end
+endmodule
